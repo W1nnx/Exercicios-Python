7fad3d52df5a206a1bb7183223dc48b299abccd8
W1nnx <90180092+W1nnx@users.noreply.github.com>
2021-09-14 05:36:13 -0300

    Delete Tesoura Papel Pedra.py

--- Tesoura Papel Pedra.py
+++
@@ -1,5 +0,0 @@
-
-jogador1 = str(input('Qual voce quer? (PEDRA / PAPEL / TESOURA) '))
-jogador2 = str(input('Qual voce quer? (PEDRA / PAPEL / TESOURA) '))
-
-if jogador1 == 'Pedra' and jogador2 == 'Tesoura':
